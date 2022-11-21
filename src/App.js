d493e411ad30e1549c5b3fada40ae1b3a6b6d67b
Matheus Abrahão <matheus.alvares23@outlook.com>
2022-11-21 10:22:04 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import Rea  t, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Header from './components/ui/Header';
 import CharacterGrid from './components/characters/CharacterGrid';
